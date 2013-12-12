db0715bb643b5c33512fb7e2d12f69c0580c71a6
wisteriaflash <wisteriaflash@163.com>
2013-12-12 18:15:26 +0800

    add base64 test

--- d3test/t1.js
+++ d3test/t1.js
@@ -126,13 +126,13 @@ var svgObj = {
           .attr('cx', function(d){return x_scale(d.age)})
           .attr('cy', function(d){return y_scale(d.height)})
           .attr('r',5);
-        //坐标-x
+        //axis-x
         var x_axis = d3.svg.axis().scale(x_scale);
         me.svg.append('g')
               .attr('class', 'x__axis')
               .attr('transform','translate(0,280)')
               .call(x_axis);
-        //-y
+        //axis-y
         var y_axis = d3.svg.axis().scale(y_scale).orient('left');
         d3.select('svg').append('g')
               .attr('class','y__axis')
@@ -171,8 +171,50 @@ var svgObj = {
         
     }
 }
+//test
+var test = {
+    init: function(){
+        var aa = [{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596688},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596676},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596668},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596534},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596458},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596451},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596346},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596340},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596334},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596279},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596273},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"2","time":1386596129},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595923},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595845},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595839},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595833},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595826},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595195},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595187},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595181},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595174},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595164},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595099},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595091},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595082},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595076},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595070},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595058},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595049},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386595043}];
+        var awardlist = ["谢谢你","50Q币","AK47","1Q币","防弹头盔","1Q币","高爆手雷","100Q币"];
+        var addAwardList = [{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596688},
+        {"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596676},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596668},{"uid":"108214169","nick":"14**09","lottery_code":"1","success_code":"1","time":1386596534},];
+        var listNum = 5;
+
+        _getAwardList(aa);
+        function _getAwardList(info){
+          //filter-data
+          var arr = [];
+          var item, _single;
+          for(var j=0, len=info.length; j<len; j++){
+            if(arr.length==listNum){
+              break;
+            }
+            item = info[j];
+            if(item["success_code"]>1){
+              arr.push(item);
+            }
+          }
+          var _listinfo = "";
+          for(var i=0; i<listNum; i++){
+            if(i<arr.length){
+              _single = arr[i];
+            }else{
+              _single = addAwardList[i-arr.length];
+            }
+            var _time = new Date(_single["time"]*1000),
+              _month = _time.getMonth()-0+1>9 ? _time.getMonth()-0+1 : "0"+(_time.getMonth()-0+1),
+              _day = _time.getDate()-0+1>9 ? _time.getDate()-0 : "0"+(_time.getDate()-0),
+              _minutes = _time.getMinutes()>9 ? _time.getMinutes() : "0"+_time.getMinutes(),
+              _hours = _time.getHours()>9 ? _time.getHours() : "0"+_time.getHours();
+            //
+            _listinfo += "<li>恭喜<em>"+ _single["nick"] + "</em>获得 " + awardlist[_single["success_code"]-1]+'<span class="time">'+_month +"-" + _day + "<em>"+_hours +":"+ _minutes +"</em></span><li>";
+          }
+          $("#J_awardList ul").html(_listinfo);
+        }
+    }
+}
 
 //init
 svgObj.init();
-
+test.init();
 });
